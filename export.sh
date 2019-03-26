93ea4e3d9b3e8ade4e85c37cbcf125d597809a6d
lollonet <claudio.loletti@gmail.com>
2019-03-26 21:45:51 +0000

    Updated to docker-ce e kubernetes last versions

---
+++ export.sh
@@ -0,0 +1,9 @@
+export EXTERNAL_NETWORK=public1
+export FLOATING_IP_NETWORK_UUID=eb727295-5bf6-4159-8fc6-58e81ca1048a
+export INCLUDE_HELM=True
+export KEY=mykey
+export IMAGE=ubuntu-16.04-LTS
+export NODE_COUNT=2
+export NAME=k8los
+
+export NODE_AUTO_IP=True

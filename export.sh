a9dfa933ec28a257f75bf58ca46641340cc5af12
lollonet <claudio.loletti@gmail.com>
2019-03-27 22:17:51 +0000

    still to be fixed, dashboard and coredns crash!

--- export.sh
+++ export.sh
@@ -1,6 +1,6 @@
 export EXTERNAL_NETWORK=public1
 export FLOATING_IP_NETWORK_UUID=eb727295-5bf6-4159-8fc6-58e81ca1048a
-export INCLUDE_HELM=True
+export INCLUDE_HELM=False
 export KEY=mykey
 export IMAGE=ubuntu-16.04-LTS
 export NODE_COUNT=2
